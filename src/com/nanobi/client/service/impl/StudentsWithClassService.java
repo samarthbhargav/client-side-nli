ca8d8297e6be0bdcebfc0fa7b1927e02e6a73dc7
Samarth Bhargav <samarth.bhargav92@gmail.com>
2014-04-02 23:14:09 +0530

    added new services

---
+++ src/com/nanobi/client/service/impl/StudentsWithClassService.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.nanobi.client.service.impl;
+
+import java.util.List;
+
+import com.nanobi.client.service.IService;
+import com.nanobi.client.service.model.ServiceRequest;
+import com.nanobi.client.service.model.ServiceResponse;
+
+/**
+ * @author hduser
+ *
+ */
+public class StudentsWithClassService implements IService {
+
+	/* (non-Javadoc)
+	 * @see com.nanobi.client.service.IService#service(com.nanobi.client.service.model.ServiceRequest)
+	 */
+	@Override
+	public ServiceResponse service(ServiceRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nanobi.client.service.IService#getResponseAsString(com.nanobi.client.service.model.ServiceRequest)
+	 */
+	@Override
+	public String getResponseAsString(ServiceRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nanobi.client.service.IService#getParamMap()
+	 */
+	@Override
+	public List<String> getParamMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/nanobi/client/service/impl/SubjectWiseService.java
@@ -0,0 +1,29 @@
+package com.nanobi.client.service.impl;
+
+import java.util.List;
+
+import com.nanobi.client.service.IService;
+import com.nanobi.client.service.model.ServiceRequest;
+import com.nanobi.client.service.model.ServiceResponse;
+
+public class SubjectWiseService implements IService{
+
+	@Override
+	public ServiceResponse service(ServiceRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getResponseAsString(ServiceRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getParamMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
