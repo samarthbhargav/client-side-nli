51cc549fe1909127d66a393b8cdce9c751b805db
Samarth Bhargav <samarth.bhargav92@gmail.com>
2014-04-10 22:03:28 +0530

    student format changed

--- src/com/nanobi/client/utils/Utils.java
+++ src/com/nanobi/client/utils/Utils.java
@@ -11,53 +11,60 @@ import com.nanobi.client.communication.TranslationResult;
 import com.nanobi.client.constants.StudentDaoConstants;
 import com.nanobi.client.model.Student;
 
+
 /**
  * @author hduser
  *
  */
-public class Utils {
-	public static ArrayList<String> constructMappingFromResult(TranslationResult result) {
-		ArrayList<String> mappings = new ArrayList<String>();
-		
-		for(String s: result.getDimensions().values()) {
-			mappings.add(s);
-		}
-		
-		for(String s: result.getMeasures().values()) {
-			mappings.add(s);
-		}
-		
-		for(String s: result.getFilters().values()) {
-			mappings.add(s);
-		}
-		
-		for(String s: result.getVisualizations().values()) {
-			mappings.add(s);
-		}
-		
-		return mappings;
-	}
-	
-	
-	public static String formatStudent(Student s) {
-	    StringBuilder builder = new StringBuilder();
-        builder.append( "<tr>" );
-        builder.append( td(s.getName()) );
-        builder.append( td(s.getUsn()) );
-        ArrayList<String> allsubjects  = new ArrayList<String>(s.getSubjectsWithSuffix( StudentDaoConstants.TOTAL_SUFFIX ));
-        Collections.sort( allsubjects );
-        for(String subj : allsubjects) {
-            builder.append( td(s.getScore( subj ).toString()) );
+public class Utils
+{
+    public static ArrayList<String> constructMappingFromResult( TranslationResult result )
+    {
+        ArrayList<String> mappings = new ArrayList<String>();
+
+        for ( String s : result.getDimensions().values() ) {
+            mappings.add( s );
         }
-        builder.append( td(s.getTotal().toString()) );
-        builder.append( td(s.getPercentage( StudentDaoConstants.MARKS_MAX ).toString()) );
-        builder.append( td(s.getResultClass().toString()) );
+
+        for ( String s : result.getMeasures().values() ) {
+            mappings.add( s );
+        }
+
+        for ( String s : result.getFilters().values() ) {
+            mappings.add( s );
+        }
+
+        for ( String s : result.getVisualizations().values() ) {
+            mappings.add( s );
+        }
+
+        return mappings;
+    }
+
+
+    public static String formatStudent( Student s )
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append( "<tr>" );
+        builder.append( tdColspan( strong( "Name: " ) + s.getName(), 2 ) );
+        builder.append( tdColspan( strong( "USN: " ) + s.getUsn(), 2 ) );
+        builder.append( tdColspan( strong( "Total: " ) + s.getTotal().toString(), 2 ) );
+        builder.append( tdColspan( strong( "Percentage: " ) + s.getPercentage( StudentDaoConstants.MARKS_MAX ).toString(), 2 ) );
+        builder.append( tdColspan( strong( "Class: " ) + s.getResultClass().toString(), 1 ) );
         builder.append( "</tr>" );
+        builder.append( "\n<tr>" );
+        ArrayList<String> allsubjects = new ArrayList<String>( s.getSubjectsWithSuffix( StudentDaoConstants.TOTAL_SUFFIX ) );
+        Collections.sort( allsubjects );
+        for ( String subj : allsubjects ) {
+            builder.append( td( strong(subj) + s.getScore( subj ).toString() ) );
+        }
+        builder.append( "</tr>\n" );
         return builder.toString();
-	}
-	
-	public static String getHeadersForTable(Student s) {
-	    StringBuilder builder = new StringBuilder();
+    }
+
+
+    /*public static String getHeadersForTable(Student s) {
+        StringBuilder builder = new StringBuilder();
         builder.append( "<tr>" );
         builder.append( th("Name") );
         builder.append( th("USN") );
@@ -71,13 +78,28 @@ public class Utils {
         builder.append( th("Class") );
         builder.append( "</tr>" );
         return builder.toString();
-	}
-	
-	private static String td(String str) {
-	    return "<td>" + str + "</td>";
-	}
-	
-	private static String th(String str) {
+    }*/
+
+    public static String strong( String str )
+    {
+        return "<strong>" + str + "</strong>";
+    }
+
+
+    private static String td( String str )
+    {
+        return "<td>" + str + "</td>";
+    }
+
+
+    private static String tdColspan( String str, int colspan )
+    {
+        return "<td colspan=" + colspan + " >" + str + "</td>";
+    }
+
+
+    private static String th( String str )
+    {
         return "<th>" + str + "</th>";
     }
 }
